2e3f18f2e77639e1b281f9742ce0418024a98d54
杨京鑫 <juyyang@yangjingxindeMacBook-Pro.local>
2021-06-09 17:36:02 +0800

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 56,
+  TESTNET = 97
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0xf85a4391a1cb137E61E11BE97776d6da08AF678c' // 已经修改
+// 已经修改
+export const INIT_CODE_HASH = '0xcd9a5d7e01d97f8bec492ae8da90b9a037b7ce3f37e5573c9b0c92ccdc4ebf1b'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const FEES_NUMERATOR = JSBI.BigInt(998)
+export const FEES_DENOMINATOR = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
